739dd6082435453ed2afed6cc0d5afcf32b0da6f
ycx <20281096@bjtu.edu.cn>
2024-03-25 20:55:22 +0800

    softmax回归

--- linear-regression-concise.py
+++ linear-regression-concise.py
@@ -9,11 +9,9 @@ def load_array(data_arrays, batch_size, is_train=True):  #@save
     dataset = data.TensorDataset(*data_arrays)
     return data.DataLoader(dataset, batch_size, shuffle=is_train)
 
-
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
 features, labels = d2l.synthetic_data(true_w, true_b, 1000)
-
 
 batch_size = 10
 data_iter = load_array((features, labels), batch_size)
